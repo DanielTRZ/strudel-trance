bd96aca120cd3bc7e10f07e56513ddf90be7af9a
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2026-07-27 22:38:46 +0200

    Update trance.js

--- trance.js
+++ trance.js
@@ -2,6 +2,7 @@
 // DANIEL'S TRANCE - TRIPLE WORKING SCOPE EDITION 
 // =================================================================
 
+
 // 1. BEAT & RYTM (Fundament perkusyjny: kick, główne hi-haty, akcenty i clap)
 $: stack(
   s("kick*4"),  
